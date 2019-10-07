c506f92e12a8285de64752c2f54cb61ffa5b5d62
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-07 07:56:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function editTask(e) {
     // console.log('edit task');
-    let item = e.target.parentNode.textContent;
+    let item = e.target.parentNode.text;
     console.log(item);
   }
 
