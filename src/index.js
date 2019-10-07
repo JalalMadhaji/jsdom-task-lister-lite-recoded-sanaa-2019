52d4c77b6a53c691476e8e76f84e631f9d138b84
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-07 07:46:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function sortList(list){
         let coll = [...list]
-        if(coll.length > 1){
+        if(coll.length > 2){
           coll.sort(function(a, b) {
           let textA = a.getAttribute('data-sort').toLowerCase()
           let textB = b.getAttribute('data-sort').toLowerCase()
