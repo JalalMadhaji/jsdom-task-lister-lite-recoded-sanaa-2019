071a7b4f1cfab4d27287c6465fc49a329dc98555
Jalal Al-Madhaji <jalal.almadhaji90@gmail.com>
2019-10-07 08:02:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -11,11 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
     e.target.parentNode.parentNode.removeChild(e.target.parentNode);
   }
 
-  function editTask(e) {
-    // console.log('edit task');
-    let item = e.target.parentNode.text;
-    console.log(item);
-  }
 
   // function sortList(list){
   //       let coll = [...list]
@@ -36,16 +31,12 @@ document.addEventListener("DOMContentLoaded", () => {
       let sortChar = option.getAttribute('data-sort');
       let li = document.createElement('li');
       let btn = document.createElement('button');
-      let editBtn = document.createElement('button');
       li.textContent = input.value + ' ';
       li.setAttribute('data-sort',sortChar);
       li.style.color = priority.value;
       btn.innerHTML = 'X';
       btn.addEventListener('click',deleteTask);
-      editBtn.innerHTML = "Edit";
-      editBtn.addEventListener('click',editTask);
       li.appendChild(btn);
-      li.appendChild(editBtn);
       tasks.appendChild(li);
       // let liList = tasks.getElementsByTagName('li');
       // let sortedList = sortList(liList);
